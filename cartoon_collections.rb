fae16fee8c15f86dd6444596fb1e068904de6624
Jordon Terry <jt5493@hotmail.com>
2020-01-31 22:17:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(array)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  array.any? {|x| x.length > 4}
 end
 
 def find_the_cheese# code an argument here
